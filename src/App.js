9950145118ec2fe4fa015850edecf56e28e87a97
Romano Livi <romano_livi@outlook.com>
2021-06-05 18:47:40 -0400

    Set up API and used hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import InputStock from './InputStock';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h1 className="title">Romano's Stock App</h1>
+        <div className="underline"></div>
+        <div className="input-stock">
+          <InputStock />
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/InputStock.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+const InputStock = () => {
+    const [stock, setStock] = useState('');
+
+    const fetchStock = (e) => {
+        e.preventDefault();
+        let API_KEY = 'c2tseiaad3icl6gefg00'
+        let url = `https://finnhub.io/api/v1/stock/profile2?symbol=${stock}&token=${API_KEY}`
+        
+        axios.get(url)
+        .then( resp => {
+            setStock(resp.data);
+        })
+        .catch( resp => 
+            console.log('errors')
+        );
+    };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setStock(e.target.value);
+    };
+
+
+    const stockCard = () => {
+        return (
+            <div className="header">
+                <header>
+                    <h3>{stock.ticker}</h3>
+                    <img src={stock.logo} alt={stock.name} />
+                </header>
+                <footer>
+                    <p>Name: {stock.name}</p>
+                    <p>Industry: {stock.finnhubIndustry}</p>
+                    <p>Shares Outstanding: {stock.shareOutstanding}</p>
+                    <a href={stock.weburl}>Click to see website</a>
+                </footer>
+            </div>
+            
+        )
+    }
+
+    return (
+        <article>
+            <form onSubmit={fetchStock}>
+                <label>Enter a stock to see it's information: </label>
+                <input type="text" onChange={handleChange} />
+                <button className="btn" type="submit">Submit</button>
+            </form>
+
+            <div>
+                {stockCard()}
+            </div>
+        </article>
+    )
+}
+
+export default InputStock;
